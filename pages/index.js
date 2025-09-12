035d64c53a63ff2e980871a85d20e00a9ca72fc7
developerjonas <thedeveloperjonas@gmail.com>
2025-09-12 17:11:43 +0545

    Backend Standby Mode

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {MdDelete, MdEdit, MdConfirmationNumber} from "react-icons/md";
+import axios from "axios";
 
 const index = () => {
   return <div>index</div>;
